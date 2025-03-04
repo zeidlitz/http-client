2618ec62ab738a01834672e445de21d6ce6e0a64
zeidlitz <zeidlitzerik@gmail.com>
2025-03-04 15:34:50 +0100

    Refactoring everything

--- http-client.c
+++ http-client.c
@@ -1,42 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <curl/curl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define DEFAULT_PORT 8080
+#define DEFAULT_HOST "127.0.0.1"
+
+void get_config(char **host, int *port)
+{
+  *host = getenv("HTTP_HOST");
+  if (*host == NULL)
+  {
+    *host = DEFAULT_HOST;
+  }
+
+  char *port_str = getenv("HTTP_PORT");
+  if (port_str == NULL)
+  {
+    *port = DEFAULT_PORT;
+  }
+  else
+  {
+    *port = atoi(port_str);
+    if (*port == 0)
+    {
+      fprintf(stderr, "Cannot use HTTP_PORT=%d need non-zero value\n", *port);
+    }
+  }
+}
 
 size_t write_callback(void *ptr, size_t size, size_t nmemb, void *userdata)
 {
   size_t total_size = size * nmemb;
   fwrite(ptr, size, nmemb, stdout);
   return total_size;
+}
 
+void send_http_request(const char *host, int port, const char *endpoint) {
+    int sockfd;
+    struct sockaddr_in server_addr;
+    char request[1024];
+    char response[4096];
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+    {
+        perror("Socket creation failed");
+        return;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    server_addr.sin_addr.s_addr = inet_addr(host);
+
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) 
+    {
+        perror("Connection failed");
+        close(sockfd);
+        return;
+    }
+
+    snprintf(request, sizeof(request),
+             "GET /%s HTTP/1.1\r\n"
+             "Host: %s:%d\r\n"
+             "Connection: close\r\n\r\n", endpoint, host, port);
+
+    if (send(sockfd, request, strlen(request), 0) < 0) 
+    {
+        perror("Send failed");
+        close(sockfd);
+        return;
+    }
+
+    ssize_t len = recv(sockfd, response, sizeof(response) - 1, 0);
+    if (len < 0) 
+    {
+        perror("Receive failed");
+    } 
+    else 
+    {
+        response[len] = '\0';
+        printf("Response:\n%s\n", response);
+    }
+
+    close(sockfd);
 }
 
 int main(int argc, char *argv[])
 {
-  CURL *curl;
-  CURLcode res;
-  curl = curl_easy_init();
+  char endpoint[256];
+  char *host;
+  int port;
 
-  if (!curl)
+  get_config(&host, &port);
+
+  while(1)
   {
-    fprintf(stderr, "Failed to initialize curl\n");
-    return EXIT_FAILURE;
+    fprintf(stdout, "%s:%d/", host, port);
+    fgets(endpoint, sizeof(endpoint), stdin);
+    endpoint[strcspn(endpoint, "\n")] = '\0';
+    send_http_request(host, port, endpoint);
   }
 
-  curl_version_info_data *ver = curl_version_info(CURLVERSION_NOW);
-  printf("libcurl version %u.%u.%u\n",
-       (ver->version_num >> 16) & 0xff,
-       (ver->version_num >> 8) & 0xff,
-       ver->version_num & 0xff);
-
-  curl_easy_setopt(curl, CURLOPT_URL, "localhost:8080/files/hello");
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-  res = curl_easy_perform(curl);
-  if (res != CURLE_OK)
-  {
-    fprintf(stderr, "curl_easy_perform() failed %s\n", curl_easy_strerror(res));
-    return EXIT_FAILURE;
-  }
-
-  curl_easy_cleanup(curl);
   return EXIT_SUCCESS;
 }
